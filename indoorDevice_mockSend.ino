5151cf8b76845163a9793c561934d483b59a58b7
ksprin <112401911+ksprin@users.noreply.github.com>
2025-11-17 09:36:43 -0500

    Add files via upload

---
+++ indoorDevice_mockSend.ino
@@ -0,0 +1,488 @@
+#include "LoRaRadio.h"
+#include "TimerMillis.h"
+#include <EEPROM.h>
+
+// maybe use clock interrupt to send last data to computer (serial output)
+// maybe use clock interrupt to listen to computer
+
+// rework to maybe not have delay in interupt, create finite state machine
+// switch to flag system with interupts
+
+// new commands: send s more often
+
+TimerMillis writeToComputerClock;
+TimerMillis readFromComputerClock;
+TimerMillis checkIfThere;
+TimerMillis mockClock;
+
+char temp1 = 0;
+char temp2 = 0;
+int rVal = 0;
+float vsig = 0;
+
+int pirEvents = 0;
+long temperature = 0;
+int moistureLevel = 0;
+int buzzerState = 0; // init on 0: buzzer off
+int buzzerOverride = 0;
+int dry = 0;
+int wet = 100;
+
+char dataComputer = 'x';
+char readVal;
+
+// for summary: EEPROM len 4096 (send dry, wet, buzzerState, buzzerOverride from data point)
+int buildingSummary; // 1 or 0
+
+// need to break down tempAvg into bytes: long is 4 bytes
+long tempAvg; // addr 0
+long tempMin; // addr 1
+long tempMax; // addr 2
+
+char tempAvg_b1;
+int tempAvg_b1_addr = 0;
+char tempAvg_b2;
+int tempAvg_b2_addr = 1;
+char tempAvg_b3;
+int tempAvg_b3_addr = 2;
+char tempAvg_b4;
+int tempAvg_b4_addr = 3;
+
+char tempMin_b1;
+int tempMin_b1_addr = 4;
+char tempMin_b2;
+int tempMin_b2_addr = 5;
+char tempMin_b3;
+int tempMin_b3_addr = 6;
+char tempMin_b4;
+int tempMin_b4_addr = 7;
+
+char tempMax_b1;
+int tempMax_b1_addr = 8;
+char tempMax_b2;
+int tempMax_b2_addr = 9;
+char tempMax_b3;
+int tempMax_b3_addr = 10;
+char tempMax_b4;
+int tempMax_b4_addr = 11;
+
+int moistureAvg; 
+int moistureAvg_addr = 12;
+
+int moistureMin; 
+int moistureMin_addr = 13;
+
+int moistureMax; 
+int moistureMax_addr = 14;
+
+int numOfDetec; 
+int numOfDetec_addr = 15;
+
+int numOfDatapoints; 
+int numOfDatapoints_addr = 16;
+
+int estabMinMax; 
+int estabMinMax_addr = 17;
+
+int dataPointForAvg;
+int dataPointForAvg_addr = 18;
+
+int needToCommunicate;
+
+int readBuzzerFlag; // flag to know if when to communicate with computer about buzzer
+int receiveFlag; // flag to know if something was received
+int checkIfThereFlag;
+
+// Char codes
+// x - null
+// r - read from computer serial port
+// n - no read
+
+void readFromComputerClockHandler(void){
+  readBuzzerFlag = 1;
+}
+
+void checkIfThereClockHandler(void){
+  checkIfThereFlag = 1;
+}
+
+void mockSend(void){
+  receiveFlag = 1;
+
+  pirEvents = 1;
+  moistureLevel = 50;
+  temperature = 10000;
+  buzzerState = 1;
+  buzzerOverride = 1;
+  wet = 83;
+  dry = 5;
+}
+
+/*
+static void receiveCallback(void){
+  if ((LoRaRadio.parsePacket() == 10) &&
+      (LoRaRadio.read() == 'S') &&
+      (LoRaRadio.read() == 'P'))
+  {
+    pirEvents = LoRaRadio.read();
+    moistureLevel = LoRaRadio.read();
+    temp1 = LoRaRadio.read();
+    temp2 = LoRaRadio.read();
+    buzzerState = LoRaRadio.read();
+    buzzerOverride = LoRaRadio.read();
+    wet = LoRaRadio.read();
+    dry = LoRaRadio.read();
+    //Serial.println("Received signal: ");
+    //Serial.print("Detections: ");
+    //Serial.println(pirEvents);
+    //Serial.print("Moisture Data: ");
+    //Serial.println(moistureLevel);
+    // got signal so send confirm
+
+    LoRaRadio.beginPacket();
+    LoRaRadio.write('O');
+    LoRaRadio.write('K');
+    LoRaRadio.write(dataComputer);
+    LoRaRadio.endPacket();
+    
+    rVal = 0;
+    rVal |= ((int)temp1) << 8;
+    rVal |= ((int)temp2);
+
+    vsig = (3.3/1023)*rVal;
+    temperature = (vsig)/((3.3 - vsig)/10000);
+    receiveFlag = 1;
+    //LoRaRadio.receive();
+  }
+  else{
+    //if (!LoRaRadio.busy()){
+      LoRaRadio.beginPacket();
+      LoRaRadio.write('N');
+      LoRaRadio.write('O');
+      LoRaRadio.write('x');
+      LoRaRadio.endPacket();
+      //Serial.println("sent NO");
+    //}
+    //LoRaRadio.receive();
+  //Serial.println("sent2");
+  }
+}
+
+static void transmitCallback(void){
+  LoRaRadio.receive();
+}
+*/
+
+void setup() {
+  Serial.begin(9600);
+    
+  while (!Serial) { }
+
+  //LoRaRadio.begin(915000000);
+
+  //LoRaRadio.setFrequency(915000000);
+  //LoRaRadio.setTxPower(14);
+  //LoRaRadio.setBandwidth(LoRaRadio.BW_125);
+  //LoRaRadio.setSpreadingFactor(LoRaRadio.SF_7);
+  //LoRaRadio.setCodingRate(LoRaRadio.CR_4_5);
+  //LoRaRadio.setLnaBoost(true);
+
+  // set up memory system
+  buildingSummary = 1; // 1 or 0
+  needToCommunicate = 0; // 1 or 0
+  readBuzzerFlag = 0;
+  receiveFlag = 0;
+  checkIfThereFlag = 0;
+  
+  tempAvg_b1 = EEPROM.read(tempAvg_b1_addr);
+  tempAvg_b2 = EEPROM.read(tempAvg_b2_addr);
+  tempAvg_b3 = EEPROM.read(tempAvg_b3_addr);
+  tempAvg_b4 = EEPROM.read(tempAvg_b4_addr);
+
+  tempMin_b1 = EEPROM.read(tempMin_b1_addr);
+  tempMin_b2 = EEPROM.read(tempMin_b2_addr);
+  tempMin_b3 = EEPROM.read(tempMin_b3_addr);
+  tempMin_b4 = EEPROM.read(tempMin_b4_addr);
+
+  tempMax_b1 = EEPROM.read(tempMax_b1_addr);
+  tempMax_b2 = EEPROM.read(tempMax_b2_addr);
+  tempMax_b3 = EEPROM.read(tempMax_b3_addr);
+  tempMax_b4 = EEPROM.read(tempMax_b4_addr);
+
+  moistureAvg = EEPROM.read(moistureAvg_addr);
+  moistureMin = EEPROM.read(moistureMin_addr);
+  moistureMax = EEPROM.read(moistureMax_addr);
+  numOfDetec = EEPROM.read(numOfDetec_addr);
+  numOfDatapoints = EEPROM.read(numOfDatapoints_addr);
+  estabMinMax = EEPROM.read(estabMinMax_addr);
+
+  dataPointForAvg = EEPROM.read(dataPointForAvg_addr);
+
+  // rebuild tempAvg, tempMin, tempMax
+  tempAvg = ((long)tempAvg_b1<<24)|((long)tempAvg_b2<<16)|((long)tempAvg_b3<<8)|(long)tempAvg_b4;
+  tempMin = ((long)tempMin_b1<<24)|((long)tempMin_b2<<16)|((long)tempMin_b3<<8)|(long)tempMin_b4;
+  tempMax = ((long)tempMax_b1<<24)|((long)tempMax_b2<<16)|((long)tempMax_b3<<8)|(long)tempMax_b4;
+
+  //LoRaRadio.onTransmit(transmitCallback);
+  //LoRaRadio.onReceive(receiveCallback);
+
+  //writeToComputerClock.start(writeToComputerClockHandler, 5000, 5000);
+  readFromComputerClock.start(readFromComputerClockHandler, 15000, 10000);
+  checkIfThere.start(checkIfThereClockHandler, 0, 5000);
+  mockClock.start(mockSend, 1000, 7000);
+  //LoRaRadio.receive(500); // might be a concern
+
+}
+
+void loop() {
+  // all writing and waiting for computer
+  if (checkIfThereFlag){
+    // send check if there
+    // Clear buffer
+    while (Serial.available() > 0){
+      Serial.read();
+    }
+
+    // need to send summary if reply
+    Serial.println("[GardenGuard]h");
+    delay(1000);
+    if (Serial.available() > 0){
+      readVal = Serial.read(); // clear the buffer
+      
+      // set flags to know the computer was checked for and reoutput the read
+      Serial.println("[GardenGuard]h|"+String(readVal));
+      checkIfThereFlag = 0;
+      // disable check if there clock
+      checkIfThere.stop();
+      delay(500); // delay after making connection
+    }
+  }
+
+  // For resetting buzzer
+  if (readBuzzerFlag && !checkIfThereFlag){
+    // Clear buffer
+    while (Serial.available() > 0){
+      Serial.read();
+    }
+
+    // send string to indicate receiving
+    Serial.println("[GardenGuard]r");
+    delay(1000);
+    if (Serial.available() > 0){
+      dataComputer = Serial.read();
+      Serial.println("[GardenGuard]r|"+String(dataComputer));
+    }
+    else{
+      Serial.println("[GardenGuard]r|n");
+      //restart clock to check if there
+      checkIfThere.start(checkIfThereClockHandler, 5000, 5000);
+      checkIfThereFlag = 1;
+    }
+    readBuzzerFlag = 0;
+  }
+
+  if (receiveFlag && !checkIfThereFlag){
+    // disable lora: need to minimize wait time
+    //LoRaRadio.receive(1500);
+    //LoRaRadio.end();
+    // check if using summary
+    if (buildingSummary){
+      // Clear buffer
+      while (Serial.available() > 0){
+        Serial.read();
+      }
+
+      // need to send summary if reply
+      Serial.println("[GardenGuard]s");
+
+      delay(1000);
+      if (Serial.available() > 0){
+        readVal = Serial.read(); // clear the buffer
+
+        // set flags for summary and send
+        needToCommunicate = 1;
+        buildingSummary = 1;
+      }
+      else{
+        // set flags for summary
+        buildingSummary = 1;
+        needToCommunicate = 0;
+
+        //restart clock to check if there
+        checkIfThere.start(checkIfThereClockHandler, 5000, 5000);
+        checkIfThereFlag = 1;
+      }
+    }
+    else{
+      // Clear buffer
+      while (Serial.available() > 0){
+        Serial.read();
+      }
+
+      Serial.println("[GardenGuard]d");
+      // just send data if reply
+      delay(1000);
+      if (Serial.available() > 0){
+        readVal = Serial.read(); // clear the buffer
+        
+        // set flags for send
+        needToCommunicate = 1;
+        buildingSummary = 0;
+      }
+      else{
+        // set flags for summary
+        buildingSummary = 1;
+        needToCommunicate = 0;
+
+        //restart clock to check if there
+        checkIfThere.start(checkIfThereClockHandler, 5000, 5000);
+        checkIfThereFlag = 1;
+      }
+    }
+
+    // write to mem is needed
+    if (buildingSummary){
+      // add to summary
+      tempAvg_b1 = EEPROM.read(tempAvg_b1_addr);
+      tempAvg_b2 = EEPROM.read(tempAvg_b2_addr);
+      tempAvg_b3 = EEPROM.read(tempAvg_b3_addr);
+      tempAvg_b4 = EEPROM.read(tempAvg_b4_addr);
+
+      tempMin_b1 = EEPROM.read(tempMin_b1_addr);
+      tempMin_b2 = EEPROM.read(tempMin_b2_addr);
+      tempMin_b3 = EEPROM.read(tempMin_b3_addr);
+      tempMin_b4 = EEPROM.read(tempMin_b4_addr);
+
+      tempMax_b1 = EEPROM.read(tempMax_b1_addr);
+      tempMax_b2 = EEPROM.read(tempMax_b2_addr);
+      tempMax_b3 = EEPROM.read(tempMax_b3_addr);
+      tempMax_b4 = EEPROM.read(tempMax_b4_addr);
+
+      moistureAvg = EEPROM.read(moistureAvg_addr);
+      moistureMin = EEPROM.read(moistureMin_addr);
+      moistureMax = EEPROM.read(moistureMax_addr);
+      numOfDetec = EEPROM.read(numOfDetec_addr); 
+      numOfDatapoints = EEPROM.read(numOfDatapoints_addr); 
+      estabMinMax = EEPROM.read(estabMinMax_addr);
+      dataPointForAvg = EEPROM.read(dataPointForAvg_addr);
+
+      // rebuild tempAvg, tempMin, tempMax
+      tempAvg = ((long)tempAvg_b1<<24)|((long)tempAvg_b2<<16)|((long)tempAvg_b3<<8)|(long)tempAvg_b4;
+      tempMin = ((long)tempMin_b1<<24)|((long)tempMin_b2<<16)|((long)tempMin_b3<<8)|(long)tempMin_b4;
+      tempMax = ((long)tempMax_b1<<24)|((long)tempMax_b2<<16)|((long)tempMax_b3<<8)|(long)tempMax_b4;
+
+      numOfDetec = numOfDetec + pirEvents;
+      numOfDatapoints = numOfDatapoints + 1;
+
+      if (estabMinMax == 0){
+        // min and max are null so put in intit values
+        tempMin = temperature;
+        tempMax = temperature;
+        moistureMin = moistureLevel;
+        moistureMax = moistureLevel;
+        tempAvg = temperature;
+        moistureAvg = moistureLevel;
+
+        estabMinMax = 1;
+      }
+      else{
+        // min and max can be updated
+        if (temperature < tempMin){
+          tempMin = temperature;
+        }
+        else if (temperature > tempMax){
+          tempMax = temperature;
+        }
+
+        if (moistureLevel < moistureMin){
+          moistureMin = moistureLevel;
+        }
+        else if (moistureLevel > moistureMax){
+          moistureMax = moistureLevel;
+        }
+      }
+      //only add to avg if not detected
+      if (pirEvents == 0){
+        dataPointForAvg = dataPointForAvg + 1;
+        if (tempAvg == 0){
+          tempAvg = temperature;
+        }
+        else{
+          tempAvg = tempAvg * (dataPointForAvg -1)/dataPointForAvg + temperature / dataPointForAvg;
+          if (tempAvg == 0){
+            tempAvg = temperature;
+          }
+        }
+        if (moistureAvg == 0){
+          moistureAvg = moistureLevel;
+        }
+        else{
+          moistureAvg = moistureAvg * (dataPointForAvg -1)/dataPointForAvg + moistureLevel / dataPointForAvg;
+          if (moistureAvg == 0){
+            moistureAvg = moistureLevel;
+          }
+        }
+      }
+
+      // break down tempAvg, tempMin, tempMax
+      tempAvg_b1 = (tempAvg >> 24);
+      tempAvg_b2 = (tempAvg >> 16);
+      tempAvg_b3 = (tempAvg >> 8);
+      tempAvg_b4 = (tempAvg);
+
+      tempMin_b1 = (tempMin >> 24);
+      tempMin_b2 = (tempMin >> 16);
+      tempMin_b3 = (tempMin >> 8);
+      tempMin_b4 = (tempMin);
+
+      tempMax_b1 = (tempMax >> 24);
+      tempMax_b2 = (tempMax >> 16);
+      tempMax_b3 = (tempMax >> 8);
+      tempMax_b4 = (tempMax);
+
+      // write to memory
+      EEPROM.write(tempAvg_b1_addr, tempAvg_b1);
+      EEPROM.write(tempAvg_b2_addr, tempAvg_b2);
+      EEPROM.write(tempAvg_b3_addr, tempAvg_b3);
+      EEPROM.write(tempAvg_b4_addr, tempAvg_b4);
+
+      EEPROM.write(tempMin_b1_addr, tempMin_b1);
+      EEPROM.write(tempMin_b2_addr, tempMin_b2);
+      EEPROM.write(tempMin_b3_addr, tempMin_b3);
+      EEPROM.write(tempMin_b4_addr, tempMin_b4);
+
+      EEPROM.write(tempMax_b1_addr, tempMax_b1);
+      EEPROM.write(tempMax_b2_addr, tempMax_b2);
+      EEPROM.write(tempMax_b3_addr, tempMax_b3);
+      EEPROM.write(tempMax_b4_addr, tempMax_b4);
+
+      EEPROM.write(moistureAvg_addr, moistureAvg); // addr 3
+      EEPROM.write(moistureMin_addr, moistureMin); // addr 4
+      EEPROM.write(moistureMax_addr, moistureMax); // addr 5
+      EEPROM.write(numOfDetec_addr, numOfDetec); // addr 6
+      EEPROM.write(numOfDatapoints_addr, numOfDatapoints); // addr 7
+      EEPROM.write(estabMinMax_addr, 1); // add 8, min and max have values
+      EEPROM.write(dataPointForAvg_addr, dataPointForAvg);
+    }
+    if (needToCommunicate){
+      if (buildingSummary){
+        // send summary
+        // 0,13625,0,1,1,0,100,0,1481,13957,0,0,5,0,3
+        Serial.println("[GardenGuard]s|"+String(moistureLevel)+","+String(temperature)+","+String(pirEvents)+","+String(buzzerState)+","+String(buzzerOverride)+","+String(dry)+","+String(wet)+","+String(tempAvg)+","+String(tempMin)+","+String(tempMax)+","+String(moistureAvg)+","+String(moistureMin)+","+String(moistureMax)+","+String(numOfDetec)+","+String(numOfDatapoints));
+
+        // communicated with computer: clear summary and set to false
+        buildingSummary = 0;
+
+        for (int i = 0; i <= 17; i++){
+          EEPROM.write(i, 0);
+        }
+      }
+      else{
+        // send stnd data
+        Serial.println("[GardenGuard]d|"+String(moistureLevel)+","+String(temperature)+","+String(pirEvents)+","+String(buzzerState)+","+String(buzzerOverride)+","+String(dry)+","+String(wet));
+      }
+
+    }
+    receiveFlag = 0;
+  }
+}
---
+++ outdoorDevice_Schedule12Sec.ino
@@ -0,0 +1,319 @@
+#include "LoRaRadio.h"
+#include "TimerMillis.h"
+#include "STM32L0.h"
+
+TimerMillis clock;
+TimerMillis buzzerClock;
+
+bool readyToPrint = false;
+bool sending = false;
+bool received = false;
+int detected;
+int moistureLevel;
+int temperature;
+int buzzerState; 
+int buzzerOverride;
+int calibrationState; 
+int dry;
+int wet;
+int sleepTime;
+int pirStatus;
+int pirFlag;
+char temp1;
+char temp2;
+int overrideSwitch;
+
+int releaseTime;
+int pressTime;
+
+int calibrateFlag;
+int buzzerFlag;
+
+long nextSchedule;
+long lastSchedule;
+
+int remainingSleep;
+
+#define STATE_INIT        0
+#define STATE_DRY    1
+#define STATE_WET   2
+#define STATE_ENDING  3
+
+char dataComputer = 'x'; // x is default char
+// Send information on Interupt
+// Only activating send (with LoRaRadio.receive)
+
+static void calibrate( void ){
+  calibrationState = STATE_INIT;
+  digitalWrite(PIN_LED2, HIGH);
+  digitalWrite(12, HIGH);
+
+  // hook up seperate button interupts
+  attachInterrupt(6, calibrateButton, CHANGE); // can only have one interupt so will need to check
+
+  while(calibrationState < STATE_ENDING){
+    if (calibrationState == STATE_DRY){
+      dry = analogRead(16) / 10;
+      
+    }
+    if (calibrationState == STATE_WET){
+      wet = analogRead(16) / 10;
+      
+    }
+  }
+  // ending
+  digitalWrite(PIN_LED2, LOW);
+  digitalWrite(12, LOW);
+  detachInterrupt(6);
+
+}
+
+void calibrateButton( void ){
+  int status = digitalRead(6);
+
+  if (status == HIGH){
+    // pressed
+    pressTime = millis();
+  }
+  else{
+    // open
+    releaseTime = millis();
+
+    //calc press time: accept if over 0.25 second
+    if ((releaseTime - pressTime) >= 60){
+      // button press: change state
+      calibrationState = calibrationState + 1;
+    }
+  }
+}
+
+void userButton( void ){
+  int status = digitalRead(6);
+  
+  if (status == LOW){
+    // pressed
+    pressTime = millis();
+  }
+  else{
+    // open
+    releaseTime = millis();
+
+    int diff =  releaseTime - pressTime;
+    //calc press time: accept if over 0.25 second
+    if (diff >= 60){
+      // long press
+      calibrateFlag = !calibrateFlag;
+    }
+    STM32L0.wakeup();
+  }
+}
+
+// Sending function
+static void send ( void ) {
+  if (sending){
+    if (!LoRaRadio.busy()){
+
+      LoRaRadio.beginPacket();
+      LoRaRadio.write('S');
+      LoRaRadio.write('P');
+      LoRaRadio.write(detected);
+      LoRaRadio.write(moistureLevel);
+      LoRaRadio.write(temp1);
+      LoRaRadio.write(temp2);
+      LoRaRadio.write(buzzerState);
+      LoRaRadio.write(buzzerOverride);
+      LoRaRadio.write(wet);
+      LoRaRadio.write(dry);
+      LoRaRadio.endPacket();
+ 
+      sending = false;
+    }
+  }
+  delay(10);
+  
+  if (LoRaRadio.parsePacket() == 3){
+     if ((LoRaRadio.read() == 'O') &&
+      (LoRaRadio.read() == 'K')){
+        // revceived confirmation
+        received = true;
+        dataComputer = LoRaRadio.read();
+        // i : buzzer on
+        // o : buzzer off
+        if (dataComputer == 'i'){
+          buzzerState = 1;
+        }
+        else if (dataComputer == 'o'){
+          buzzerState = 0;
+        }
+        //Serial.println("Indoor device received last message");
+      }
+  }
+  
+
+}
+
+static void transmitCallback ( void ){
+  LoRaRadio.receive(500);
+}
+
+
+void pirInterupt(void){
+  // dont't have wake up: just increment detec and have buzzer go off
+  detected = detected + 1;
+
+  overrideSwitch = digitalRead(9);
+  if (overrideSwitch){
+    //Serial.println("buzzer override");
+    buzzerOverride = 1;
+    buzzerFlag = 0;
+  }
+  else{
+    buzzerOverride = 0;
+    buzzerFlag = 1;
+  }
+
+  if (!buzzerOverride && buzzerState){
+      //Serial.println("BUZZ")
+
+      digitalWrite(8, HIGH);
+      delay(1000);
+      digitalWrite(8, LOW);
+
+  }
+
+}
+
+void setup( void ) {
+  
+    Serial.begin(9600);
+    
+    while (!Serial) { }
+  
+    LoRaRadio.begin(915000000);
+
+    LoRaRadio.setFrequency(915000000);
+    LoRaRadio.setTxPower(14);
+    LoRaRadio.setBandwidth(LoRaRadio.BW_125);
+    LoRaRadio.setSpreadingFactor(LoRaRadio.SF_7);
+    LoRaRadio.setCodingRate(LoRaRadio.CR_4_5);
+    LoRaRadio.setLnaBoost(true);
+
+    detected = 0;
+    moistureLevel = 0;
+    buzzerState = 1; // init as Off
+    buzzerOverride = 0;
+    temperature = 0;
+    calibrationState = 0;
+    dry = 0;
+    wet = 100;
+    releaseTime = 0;
+    pressTime = 0;
+    calibrateFlag = 0;
+    buzzerFlag = 0;
+    sleepTime = 12000;
+    pirStatus = 0;
+    pirFlag = 0;
+    overrideSwitch = 0;
+
+    nextSchedule = 0;
+    lastSchedule = 0;
+    remainingSleep = 20000;
+
+    // set output on PA_8: GPIO out (7) : thermistor power
+    pinMode(7, OUTPUT); 
+
+    // Thermistor A2 (18)
+    pinMode(18, INPUT);
+
+    // set output on PA_9: GPIO out (8) : buzzer in
+    // buzzer power is 3.3V
+    pinMode(8, OUTPUT);
+
+    // set interupt on PB_15: GPIO in (11) : PIR output
+    pinMode(11, INPUT); 
+
+    // Moisture A0 (16)
+    pinMode(16, INPUT); 
+
+    // Moisture Sensor Power: PB_14: GPIO out (12)
+    pinMode(12, OUTPUT);
+
+    // Switch power: PB_13: GPIO out (13)
+    pinMode(13, OUTPUT);
+
+    // Switch in: PB_12: GPIO in (9)
+    // Buzzer is override (off) with on (switch high)
+    pinMode(9, INPUT);
+
+    // User Button
+    pinMode(6, INPUT);
+
+    // LED
+    pinMode(PIN_LED2, OUTPUT);
+
+    LoRaRadio.onTransmit(transmitCallback);
+    LoRaRadio.onReceive(send);
+    attachInterrupt(11, pirInterupt, FALLING);
+    attachInterrupt(6, userButton, CHANGE);
+    digitalWrite(PIN_LED2, LOW);
+    digitalWrite(13, HIGH);
+
+}
+
+void loop() {
+  detachInterrupt(11);
+  detachInterrupt(6);
+  //Serial.print("detected: ");
+  //Serial.println(detected);
+
+  if (calibrateFlag){
+    // call calibrate method
+    //Serial.println("calibrate");
+    calibrate();
+    calibrateFlag = 0;
+  }
+
+  overrideSwitch = digitalRead(9);
+  if (overrideSwitch){
+    //Serial.println("buzzer override");
+    buzzerOverride = 1;
+    buzzerFlag = 0;
+  }
+  else{
+    buzzerOverride = 0;
+    buzzerFlag = 1;
+  }
+  
+  // can comment when no sensors attatched
+  digitalWrite(12, HIGH);
+  digitalWrite(7, HIGH);
+  delay(250);
+  moistureLevel = analogRead(16) / 10;
+  temperature = analogRead(18);
+  digitalWrite(12, LOW);
+  digitalWrite(7, LOW);
+
+  temp1 = (char) (temperature>>8);
+  temp2 = (char) (temperature);
+
+  sending = true;
+  received = false;
+  LoRaRadio.receive(10);
+  
+  //sending = false;
+  delay(1000); //delay to send can probably make less time 
+
+  if (!received){
+    sending = true;
+    LoRaRadio.receive(10);
+    delay(1000);
+  }
+
+  detected = 0; //reset
+  sending = false;
+  
+  attachInterrupt(11, pirInterupt, FALLING);
+  attachInterrupt(6, userButton, CHANGE);
+  STM32L0.sleep(sleepTime); // calls LoRaRadio.receive for some reason
+  
+}
